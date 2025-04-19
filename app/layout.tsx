6f4e0a87cd3d07c1ebc97bdbf5181a5c8fcdde43
Julie <7164072+jolisdegats@users.noreply.github.com>
2025-04-19 21:34:25 +0200

    metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,40 @@ const inter = Inter({ subsets: ["latin"] });
 const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Page Transition",
-  description: "Using framer-motion to add trasition between pages",
+  title: "Jolisdegats' Portfolio",
+  description: "Ideas, Code and Coffee",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
+  },
+  openGraph: {
+    title: "Jolisdegats' Portfolio",
+    description: "Ideas, Code and Coffee",
+    url: 'https://jolisdegats.dev',
+    siteName: "Jolisdegats' Portfolio",
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: "Jolis Degats Preview",
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Jolisdegats' Portfolio",
+    description: "Ideas, Code and Coffee",
+    images: ['/og-image.png'],
+  },
 };
 
 export default function RootLayout({
