a11eff2453cb11dcf18dd682e980a088efb586c4
Julie <7164072+jolisdegats@users.noreply.github.com>
2025-04-22 18:40:24 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.(mp3)$/,
-            use: {
-              loader: 'file-loader',
-              options: {
-                publicPath: '/_next/static/sounds/',
-                outputPath: 'static/sounds/',
-                name: '[name].[ext]',
-                esModule: false,
-              },
-            },
-          });
-      return config;
-    },
+  productionBrowserSourceMaps: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[name][ext]'
+      }
+    });
+    return config;
+  },
+  images: {
+    formats: ['image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+  },
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ['three', 'motion'],
+  },
 };
-
-
 
 export default nextConfig;
